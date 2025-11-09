86a5088bf02f914301cab8780b5fb6508aba8b3b
Noah O'Neill <135152101+drowsap@users.noreply.github.com>
2025-11-08 21:10:45 -0500

    I got a button working with canvas

--- Sleep Duster/Assets/Scripts/MoveToScene.cs
+++ Sleep Duster/Assets/Scripts/MoveToScene.cs
@@ -1,14 +1,8 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
-public class TouchButton : MonoBehaviour
+public class MoveToScene : MonoBehaviour
 {
-    public SpriteRenderer render;
-
-    public GameObject something;
-
     public string nextScene;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,10 +14,10 @@ public class TouchButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        render.color = Color.green;
+        
     }
 
-    public void Touch(InputAction.CallbackContext context)
+    public void ChangeScene()
     {
         SceneManager.LoadScene(nextScene);
     }
