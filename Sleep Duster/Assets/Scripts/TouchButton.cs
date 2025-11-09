af2cba8291552e14482221f6856a167227786775
Noah O'Neill <135152101+drowsap@users.noreply.github.com>
2025-11-08 19:23:32 -0500

    Unsure?

--- Sleep Duster/Assets/Scripts/TouchButton.cs
+++ Sleep Duster/Assets/Scripts/TouchButton.cs
@@ -5,34 +5,45 @@ public class TouchButton : MonoBehaviour
 {
     public SpriteRenderer sprite;
 
-    string nextScene;
+    public GameObject something;
+
+    public string nextScene;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      
+
+        Input.simulateMouseWithTouches = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Touch touch = Input.GetTouch(0);
 
-        if (sprite.bounds.Contains(touch.position))
+
+        if (Input.touchCount > 0)
         {
-            if (touch.phase == TouchPhase.Ended)
+            Touch tap = Input.GetTouch(0);
+
+            GameObject somethingNew = Instantiate(something);
+            somethingNew.transform.position = tap.position;
+
+
+            if (sprite.bounds.Contains(tap.position))
             {
-                SceneManager.LoadScene(nextScene);
+                if (tap.phase.Equals(TouchPhase.Ended))
+                {
+                    SceneManager.LoadScene(nextScene);
+                }
+                else
+                {
+                    sprite.color = Color.cyan;
+                }
             }
             else
             {
-                sprite.color = Color.cyan;
+                sprite.color = Color.white;
             }
         }
-        else
-        {
-            sprite.color = Color.white;
-        }
     }
-        
 }
