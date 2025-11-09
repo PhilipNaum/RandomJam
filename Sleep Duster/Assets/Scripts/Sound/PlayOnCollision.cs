c22f6012b355b8fcd148b18d42824e44493e7200
Noah O'Neill <135152101+drowsap@users.noreply.github.com>
2025-11-09 13:27:42 -0500

    I finished sound scripts for creation and collision

---
+++ Sleep Duster/Assets/Scripts/Sound/PlayOnCollision.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayOnCollision : MonoBehaviour
+{
+    public AudioSource sound;
+    public string tagName;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == tagName)
+        {
+            sound.Play();
+        }
+    }
+}
